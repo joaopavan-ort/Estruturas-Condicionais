ae0bf20ba015db22b6d1939ded78df435bc59658
joaopavan-ort <joao.pavan@ort.org.br>
2026-03-25 14:47:04 -0300

    Update Ex4.py

--- Ex4.py
+++ Ex4.py
@@ -5,19 +5,28 @@
 
 dia = int(input('Informe um nº de 1 a 7: '))
 
+while(True):
 if dia == 1:
     print(dia, 'corresponde ao domingo')
+    break
 elif dia == 2:
     print(dia, 'corresponde à segunda')
+    break
 elif dia == 3:
     print(dia, 'corresponde à terça')
+    break
 elif dia == 4:
     print(dia, 'corresponde à quarta')
+    break
 elif dia == 5:
     print(dia, 'corresponde à quinta')
+    break
 elif dia == 6:
     print(dia, 'corresponde à sexta')
+    break
 elif dia == 7:
     print(dia, 'corresponde ao sábado')
+    break
 else:
     print('Esse número é inválido.')
+    dia = int(input('Informe um nº de 1 a 7: '))
